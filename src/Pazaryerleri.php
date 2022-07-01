ece4d926c9fc88e1bd840c40e4bf03a528a22ec8
Vahdet Yavuz <vahdetyvz@gmail.com>
2022-07-01 13:09:12 +0300

    Update Pazaryerleri.php

--- src/Pazaryerleri.php
+++ src/Pazaryerleri.php
@@ -1,24 +1,8 @@
 <?php
 	namespace Vahdetyvz\PazaryeriEntegrasyon;
-	
+	use Vahdetyvz\PazaryeriEntegrasyon\Hepsiburada\Hepsiburada;
 	class Pazaryerleri {
-		public function __autoload($class) {
-			spl_autoload_register(function($class){
-				echo $class;
-				$prefix = 'TicaretPlus\\';
-				$base_dir = __DIR__ . '/src/'.$class.'/';
-				$len = strlen($prefix);
-				if (strncmp($prefix, $class, $len) !== 0){
-					return;
-				}
-				$relative_class = substr($class, $len);
-				$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-				if (file_exists($file))
-					require_once $file;
-			});
-		}
-		
 		public function __construct() {
-			
+			$hepsiburada = new Hepsiburada();
 		}
 	}
