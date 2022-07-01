bfc737811a1e4a71f92cb5118f13d0317b176f30
Vahdet Yavuz <vahdetyvz@gmail.com>
2022-07-01 17:38:28 +0300

    scsc

--- src/Hepsiburada/Services/Products.php
+++ src/Hepsiburada/Services/Products.php
@@ -1,6 +1,6 @@
 <?php 
 	namespace Vahdetyvz\PazaryeriEntegrasyon\Hepsiburada;
-	use Vahdetyvz\PazaryeriEntegrasyon\Hepsiburada;
+	use Vahdetyvz\PazaryeriEntegrasyon\Hepsiburada\Hepsiburada;
 	class Products extends Hepsiburada {
 		public $suburl = '/product/api/';
 		public function __construct() {
