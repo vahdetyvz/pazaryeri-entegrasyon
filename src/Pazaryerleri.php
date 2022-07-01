51b98b9c2f83fba9421ea1908a92812edb49c640
Vahdet Yavuz <vahdetyvz@gmail.com>
2022-07-01 12:42:17 +0300

    ddeede

--- src/Pazaryerleri.php
+++ src/Pazaryerleri.php
@@ -1,7 +1,23 @@
 <?php
-	namespace Pazaryeri;
+	namespace Vahdetyvz/PazaryeroEntegrasyon;
 	class Pazaryerleri {
+		public function __autoload($class) {
+			spl_autoload_register(function($class){
+				$prefix = 'TicaretPlus\\';
+				$base_dir = __DIR__ . '/src/libraries/';
+				$len = strlen($prefix);
+				if (strncmp($prefix, $class, $len) !== 0){
+					return;
+				}
+				$relative_class = substr($class, $len);
+				$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+				if (file_exists($file))
+					echo $file;
+					require_once $file;
+			});
+		}
+		
 		public function __construct() {
-			
+			$trendyol = new Trendyol/Trendyol();
 		}
 	}
---
+++ src/Trendyol/Trendyol.php
@@ -0,0 +1,7 @@
+<?php
+	namespace Vahdetyvz/PazaryeriEntegrasyon/Trendyol;
+	class Trendyol {
+		public function __construct() {
+			echo "qwdwq";
+		}
+	}
